1815021600752780e5eb3536579f4326372117c1
Dougie Zoraida <dzoraida@in-c6.nsk.21-school.ru>
2022-02-08 22:26:42 +0700

    view

--- src/view.c
+++ src/view.c
@@ -1,36 +1,127 @@
 #include "calc.h"
 
-void create_digit_button(GtkWidget **buttonDigits, int positionX, int positionY, GtkWidget *fixed) {
-    char* title[12] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ""};
+void create_digit_button(GtkWidget **buttonDigits, int width, int height, GtkWidget *fixed) {
+    char* title[11] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","};
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width - 4 * SizeButton - 3 * SizeSpace;
+    int positionY = height - 4 * SizeButton - 3 * SizeSpace;
     GtkWidget *button;
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 11; i++) {
         button = gtk_button_new_with_label(title[i]);
-        gtk_widget_set_size_request(button, 50, 50);
+        gtk_widget_set_size_request(button, SizeButton, SizeButton);
         gtk_fixed_put(GTK_FIXED(fixed), button, positionX, positionY);
         buttonDigits[i] = button;
-        positionX += 54;
+        positionX += SizeButton + SizeSpace;
+        if ((i + 1) % 3 == 0) {
+            positionY += SizeButton + SizeSpace;
+            positionX = width - 4 * SizeButton - 3 * SizeSpace;
+        }
     }
-    //return 1;
 }
 
+void create_function_button(GtkWidget **buttonFunctions, int width, int height, GtkWidget *fixed) {
+    char* title[9] = {"cos", "sin", "tan", "acos", "asin", "atan", "sqrt", "ln", "log"};
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width - 7 * SizeButton - 6 * SizeSpace;
+    int positionY = height - 3 * SizeButton - 2 * SizeSpace;
+    GtkWidget *button;
+    for (int i = 0; i < 9; i++) {
+        button = gtk_button_new_with_label(title[i]);
+        gtk_widget_set_size_request(button, SizeButton, SizeButton);
+        gtk_fixed_put(GTK_FIXED(fixed), button, positionX, positionY);
+        buttonFunctions[i] = button;
+        positionX += SizeButton + SizeSpace;
+        if ((i + 1) % 3 == 0) {
+            positionY += SizeButton + SizeSpace;
+            positionX = width - 7 * SizeButton - 6 * SizeSpace;
+        }
+    }
+}
+
+void create_operation_button(GtkWidget **buttonOperations, int width, int height, GtkWidget *fixed) {
+    char* title[7] = {"+/-", "^", "mod", "/", "*", "+", "-"};
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width - 4 * SizeButton - 3 * SizeSpace;
+    int positionY = height - 5 * SizeButton - 4 * SizeSpace;
+    GtkWidget *button;
+    for (int i = 0; i < 7; i++) {
+        button = gtk_button_new_with_label(title[i]);
+        gtk_widget_set_size_request(button, SizeButton, SizeButton);
+        gtk_fixed_put(GTK_FIXED(fixed), button, positionX, positionY);
+        buttonOperations[i] = button;
+        if (i < 3) 
+            positionX += SizeButton + SizeSpace;
+        if (i == 2) {
+            positionX = width - SizeButton;
+            positionY = height - 4 * SizeButton - 3 * SizeSpace; 
+        }
+        if (i > 2)
+            positionY += SizeButton + SizeSpace;
+    }
+}
+
+void create_brackets_button(GtkWidget **buttonBrackets, int width, int height, GtkWidget *fixed) {
+    char* title[2] = {"(", ")"};
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width - 7 * SizeButton - 6 * SizeSpace;
+    int positionY = height - 5 * SizeButton - 4 * SizeSpace;
+    GtkWidget *button;
+    for (int i = 0; i < 2; i++) {
+        button = gtk_button_new_with_label(title[i]);
+        gtk_widget_set_size_request(button, 3 * SizeButton + 2 * SizeSpace, SizeButton);
+        gtk_fixed_put(GTK_FIXED(fixed), button, positionX, positionY);
+        buttonBrackets[i] = button;
+        positionY += SizeButton + SizeSpace;
+    }
+}
+
+
+void create_delete_button(GtkWidget *buttonDelete, int width, int height, GtkWidget *fixed) {
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width -  SizeButton;
+    int positionY = height - 5 * SizeButton - 4 * SizeSpace;
+    buttonDelete = gtk_button_new_with_label("AC");
+    gtk_widget_set_size_request(buttonDelete, SizeButton, SizeButton);
+    gtk_fixed_put(GTK_FIXED(fixed), buttonDelete, positionX, positionY);
+}
+
+void create_equal_button(GtkWidget *buttonEqual, int width, int height, GtkWidget *fixed) {
+    int SizeButton = 60, SizeSpace = 4;
+    int positionX = width - 2 * SizeButton - SizeSpace;
+    int positionY = height - SizeButton;
+    buttonEqual = gtk_button_new_with_label("=");
+    gtk_widget_set_size_request(buttonEqual, SizeButton, SizeButton);
+    gtk_fixed_put(GTK_FIXED(fixed), buttonEqual, positionX, positionY);
+}
+
+
 void init(int argc, char *argv[]) {
-    int positionX = 0;
-    int positionY = 0;
+    int width = 444, height = 400;
+    int positionX = 0, positionY = 0;
     GtkWidget *window;
     GtkWidget *fixed;
-    GtkWidget *buttonDigits[12];
+    GtkWidget *buttonDigits[11];
+    GtkWidget *buttonEqual;
+    GtkWidget *buttonFunctions[9];
+    GtkWidget *buttonBrackets[2];
+    GtkWidget *buttonOperations[7];
+    GtkWidget *buttonDelete;
     
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Calculator");
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 700, 300);
+    gtk_window_set_default_size(GTK_WINDOW(window), width, height);
 
     fixed = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER(window), fixed);
 
-    create_digit_button(buttonDigits, positionX, positionY, fixed);
-    //GtkWidget *button = gtk_button_new_with_label(text);
+    create_digit_button(buttonDigits, width, height, fixed);
+    create_equal_button(buttonEqual, width, height, fixed);
+    create_function_button(buttonFunctions, width, height, fixed);
+    create_delete_button(buttonDelete, width, height, fixed);
+    create_operation_button(buttonOperations, width, height, fixed);
+    create_brackets_button(buttonBrackets, width, height, fixed);
     
     gtk_widget_show_all(window);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
